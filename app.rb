1f9a9fc66b4ea5b749c19f0d79a6778a9923abe4
beckireby15@gmail.com <beckireby15@gmail.com>
2022-10-18 17:56:55 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -1,2 +1,9 @@
 # app.rb
-puts "hello world!"
+puts "Hello World!\n"
+
+print "Pass this test, please."
+
+# inspecting data with p and pp method
+p [1, 2, 3] 
+## pretty printing with pp method for complex data ie nested arrays and hashes
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
